ed803bcfd6dc8fc5174f5eaf7e3bf5f85fc7e83e
Kirill-nk <94233887+Kirill-nk@users.noreply.github.com>
2023-12-10 23:56:33 +0300

    katok-tf. index.php

---
+++ index.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<META http-equiv='Content-Type' content='text/html; charset=Windows 1251'>
+<META NAME='AUTHOR' CONTENT='Kirill_Plotskiy'>
+<META NAME='DESCRIPTION' CONTENT='Каток в Новокузнецке. Открытый каток в Новокузнецке, Запсибовец'>
+<META NAME='KEYWORDS' CONTENT='katok-tf.ru, www.katok-tf.ru, http://katok-tf.ru, http://www.katok-tf.ru, Каток в Новокузнецке, Открытый каток в Новокузнецке, Каток на стадионе Запсибовец, zapsibovets.ru'>
+<META NAME='COPYRIGHT' CONTENT='Copyright (c) 2023 by WWW.KATOK-TF.RU'>
+<META http-equiv='cache-control' content='no-cache'>
+<META http-equiv='pragma' content='no-cache'>
+<META http-equiv='Content-Language' content='ru'>
+<meta http-equiv="cache-control" content="no-cache">
+<meta http-equiv="expires" content="0">
+
+
+        <title>Каток в Новокузнецке</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+
+<div id="maincontent">
+
+	<div id="head">
+    </div>
+    <div class="menu">
+    
+<ul>
+  <li class="abc"><a href="index.php">Главная</a></li>
+  <li><a href="road.html" id="current">Как проехать</a></li>
+  <li><a href="galery.html">Галерея</a></li>
+  <li><a href="rules.html">Правила катка</a></li>
+  <li><a href="sharp.html">Заточка</a></li>
+  <li><a href="contacts.html">Контакты</a></li>
+</ul>
+    
+    
+    </div>
+    <div class="content">
+
+Сегодня:  
+<?
+$today = getdate();
+$months=array("января","февраля","марта","апреля","мая","июня","июля","августа","сентября","октября","ноября","декабря");
+$month = $months[$today['mon']-1];
+$mday = $today['mday'];
+$year = $today['year'];
+echo "$mday $month $year";
+echo "г.";
+?>
+
+<br><br>
+
+
+<p><font face="Tahoma" color="#043a8a" size="5">
+
+
+<font size="4">
+
+Уважаемые посетители! Cезон 2023-2024 открыт!<br>
+
+
+<p><font face="Tahoma" color="#043a8a" size="5"><br>
+Приглашаем на Каток! </br>
+Площадь Торжеств Заводского района!!!</font>
+<br/><br/>
+<p><h2>8-905-911-9777</h2></p>
+
+
+<!--
+<font size="+2"><B>В праздничные дни 23 февраля каток работает:</B></font>
+
+<table width="500" border=1 align="center" bgcolor="" cellpadding="2" cellspacing="0">
+         <tr bgcolor="#6699ff">
+                <th align="center" ><B>Дата</B></th>        
+                <th align="center" ><B>Рабочие часы</B></th>
+         </tr>
+			<tr bgcolor="#FFD700">
+      <td align="center">23.02.2023,</br> 24.02.2023,<br>
+25.02.2023,</br> 26.02.2023</td>
+                <td align="center">12<sup>00</sup>- 22<sup>00</sup></td> 
+                
+-->			</tr>
+<!--			<tr bgcolor="#FFD700">
+      <td align="center">с 01.01.2022 по 08.01.2023</td>
+                <td align="center">12<sup>00</sup>- 22<sup>00</sup></td> 
+                
+-->			</tr>
+</table><br>
+
+
+<img src="glavn1.jpeg" width="700" height="400" alt="" />
+</br>
+
+<br/>
+</font>
+
+
+<!--
+<div id="smsaero_widget"></div>
+<script src='https://smsaero.ru/service/widget/js/TEB4ZPLOTQu4VaOWoiiQ76gzoveMRkVr' type='text/javascript'></script>
+<script>
+SMSAERO_WIDGET.init("smsaero_widget");
+</script>
+-->
+
+</br>
+<div id="vk123" align="Center">
+<script type="text/javascript" src="https://vk.com/js/api/openapi.js?169"></script>
+
+<!-- VK Widget -->
+<div id="vk_groups"></div>
+<script type="text/javascript">
+VK.Widgets.Group("vk_groups", {mode: 3, width: "400", color1: 'C8FAFC'}, 104732119);
+</script>
+</div>
+
+<br/>
+
+<p>&nbsp</p>
+
+<p>&nbsp</p>
+
+
+
+<br>
+
+<!--
+
+<table width="500" border=1 align="center" bgcolor="" cellpadding="2" cellspacing="0">
+       <tr bgcolor="#6699ff">
+                <th align="center" ><B>День недели</B></th>        
+                <th align="center" colspan="3"><B>Рабочие часы</B></th>
+        </tr>
+        <tr bgcolor="#FFD700">
+                <td bgcolor="">с 24.12.2022 до </td>
+                <td align="center" colspan="3">15<sup>00</sup>- 21<sup>00</sup></td>
+        </tr>
+        <tr bgcolor="#FFD700">
+                <td bgcolor="">14.02.2016, Воскресенье</td>
+                <td align="center" colspan="3">15<sup>00</sup>- 21<sup>00</sup></td>
+        </tr>
+        </table><br>
+
+<br>
+-->
+
+<font size="+2"><B>Режим работы катка в выходные и праздничные дни</B></font>
+        <table width="500" border=1 align="center" bgcolor="" cellpadding="2" cellspacing="0">
+        <tr bgcolor="#6699ff">
+                <th align="center" ><B>День недели</B></th>        
+                <th align="center" colspan="3"><B>Рабочие часы</B></th>
+        </tr>
+        <tr bgcolor="">
+                <td bgcolor=""><strong>Суббота</strong></td>
+                <td align="center" rowspan="3">12<sup>00</sup>- 22<sup>00</sup></td>
+                        </tr>
+        <tr bgcolor="">
+                <td bgcolor=""><strong>Воскресенье</strong></td>
+        </tr>
+        <tr bgcolor="">
+                <td bgcolor=""><strong>Праздничные дни</strong></td>
+        </tr>
+</table><br>
+
+
+<font size="+2"><B>Режим работы катка в рабочие дни</B></font>
+      <table width="500" border=1 align="center" bgcolor="" cellpadding="2" cellspacing="0">
+       <tr bgcolor="#6699ff">
+                <th align="center" ><B>День недели</B></th>        
+                <th align="center" colspan="3"><B>Рабочие часы</B></th>
+        </tr>
+        <tr bgcolor="">
+                <td bgcolor="">Понедельник</td>
+                <td align="center" colspan="3">16<sup>00</sup>- 22<sup>00</sup></td>
+        </tr>
+        <tr bgcolor="">
+                <td bgcolor="">Вторник</td>
+                <td align="center" colspan="3">16<sup>00</sup>- 22<sup>00</sup></td>
+        </tr>
+        <tr bgcolor="">
+                <td bgcolor="">Среда</td>
+                <td align="center" colspan="3">16<sup>00</sup>- 22<sup>00</sup></td>
+        </tr>
+        <tr bgcolor="">
+                <td bgcolor="">Четверг</td>
+                <td align="center" colspan="3">16<sup>00</sup>- 22<sup>00</sup></td>
+        </tr>
+        <tr bgcolor="" valign="top">
+                <td bgcolor="" height="" >Пятница</td>
+                <td align="center" colspan="3">16<sup>00</sup>- 22<sup>00</sup></td>
+        </tr>
+     </table><br>
+
+<font size="+2"><B>Цены катка сезона 2022-2023</B></font>
+<table width="500" border="1" cellspacing="0">
+<tr>
+        <td colspan="2" align="center" width="" bgcolor="#6699ff"><strong>Вход</strong></td>
+</tr>
+<tr>
+        <td>Дети</td>
+        <td align="center">150 руб.</td>
+</tr>
+<tr>
+        <td>Взрослые</td>
+        <td align="center">150 руб.</td>
+</tr>
+<tr>
+        <td>Прокат коньков</td>
+        <td align="center">200 руб./час</td>
+</tr>
+<tr>
+        <td colspan="2" align="center" width="" bgcolor="#6699ff"><strong>Заточка коньков</strong></td>
+</tr>
+<tr>
+        <td><a href="zatochka.html">Заточка коньков</a></td>
+        <td align="center">200 руб./1 пара</td>
+</tr>
+</table>
+
+
+<br>
+
+<!-- Gismeteo informer START -->
+<center>
+<link rel="stylesheet" type="text/css" href="http://www.gismeteo.ru/static/css/informer2/gs_informerClient.min.css">
+<div id="gsInformerID-5t8t6hFH3j3HJD" class="gsInformer" style="width:300px;height:243px">
+  <div class="gsIContent">
+   <div id="cityLink">
+     <a href="http://www.gismeteo.ru/city/daily/4721/" target="_blank">Погода в Новокузнецке</a>
+   </div>
+   <div class="gsLinks">
+     <table>
+       <tr>
+         <td>
+           <div class="leftCol">
+             <a href="http://www.gismeteo.ru" target="_blank">
+               <img alt="Gismeteo" title="Gismeteo" src="http://www.gismeteo.ru/static/images/informer2/logo-mini2.png" align="absmiddle" border="0" />
+               <span>Gismeteo</span>
+             </a>
+           </div>
+           <div class="rightCol">
+             <a href="http://www.gismeteo.ru/city/weekly/4721/" target="_blank">Прогноз на 2 недели</a>
+           </div>
+           </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</div>
+<script src="//www.gismeteo.ru/ajax/getInformer/?hash=5t8t6hFH3j3HJD" type="text/javascript"></script>
+<!-- Gismeteo informer END -->
+
+<br>
+
+</center>
+<br /><br />
+
+<br/><br/>
+<!--<div style="position:relative">
+<div style="position:absolute;left:-40px">
+<img src="DSC00656_1.jpg">
+</div>
+</div>-->
+<br /><br /><br /><br /><br /><br /><br /><br />
+<br /><br /><br /><br />
+<div style="width:100%;hegiht:220px">&nbsp;</div>
+
+
+<br>
+
+</div>
+</table>
+
+<!--LiveInternet counter--><script type="text/javascript"><!--
+document.write("<a href='http://www.liveinternet.ru/click' "+
+"target=_blank><img src='http://counter.yadro.ru/hit?t11.6;r"+
+escape(document.referrer)+((typeof(screen)=="undefined")?"":
+";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+";"+Math.random()+
+"' alt='' title='LiveInternet: показано число просмотров за 24"+
+" часа, посетителей за 24 часа и за сегодня' "+
+"border=0 width=88 height=31><\/a>")//--></script><!--/LiveInternet-->
+<br />
+
+<table width="100%">
+<tr>
+<td bgcolor="#e4f6fb" align="center">
+
+
+</td>
+<!--
+<td bgcolor="#e4f6fb">
+<iframe src="//widget.instagramm.ru/?imageW=2&imageH=2&thumbnail_size=107&type=0&typetext=katok_novokuznetsk&head_show=1&profile_show=0&shadow_show=0&bg=255,255,255,1&opacity=true&head_bg=4384ba&subscribe_bg=ad4141&border_color=c3c3c3&head_title=" allowtransparency="true" frameborder="0" scrolling="no" style="border:none;overflow:hidden;width:240px;height:294px;"></iframe>
+-->
+
+</td></tr>
+</table>
+
+
+<table width="100%">
+<tr>
+        <td bgcolor="#6699ff" align="center">Наши координаты: г. Новокузнецк, Заводской район, площадь Общественных мероприятий<br>
+<a href="mailto:katok-tf@yandex.ru">katok-tf@yandex.ru</a>, тел. 8-905-911-9777 справочная</td>
+</tr>
+
+</td></tr>
+</table>
+    
+    </div>
+    </div>
+
+
+</div>
+
+
+</body>
+</html>
